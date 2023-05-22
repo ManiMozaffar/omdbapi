6638b4b79bcea0ba571be43f4a9bf2ce82ca65d4
Mani <mani.mozaffar@gmail.com>
2023-05-22 10:40:59 +0300

    imrpoved: core's abstract code

--- src/core/crawler/cores.py
+++ src/core/crawler/cores.py
@@ -7,33 +7,36 @@ class HTTPXCore(AbstractCore):
         with httpx.Client() as client:
             self.headers = client.headers
 
+    async def get_session(self) -> httpx.AsyncClient:
+        return httpx.AsyncClient()
+
     async def get(self, url, params) -> httpx.Response:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(
-                url, params=params, headers=self.headers
-            )
-            return response
+        client = await self.get_session()
+        response = await client.get(
+            url, params=params, headers=self.headers
+        )
+        return response
 
     async def post(self, url, params, body) -> httpx.Response:
-        async with httpx.AsyncClient() as client:
-            response = await client.post(
-                url, params=params, json=body, headers=self.headers
-            )
-            return response
+        client = await self.get_session()
+        response = await client.post(
+            url, params=params, json=body, headers=self.headers
+        )
+        return response
 
     async def put(self, url, params, body) -> httpx.Response:
-        async with httpx.AsyncClient() as client:
-            response = await client.put(
-                url, params=params, json=body, headers=self.headers
-            )
-            return response
+        client = await self.get_session()
+        response = await client.put(
+            url, params=params, json=body, headers=self.headers
+        )
+        return response
 
     async def delete(self, url, params, body) -> httpx.Response:
-        async with httpx.AsyncClient() as client:
-            response = await client.delete(
-                url, params=params, json=body, headers=self.headers
-            )
-            return response
+        client = await self.get_session()
+        response = await client.delete(
+            url, params=params, json=body, headers=self.headers
+        )
+        return response
 
     async def add_to_header(self, key, value) -> dict:
         self.headers[key] = value
